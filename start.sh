457275278d22f4951523ad1b36e2cfb6b2665614
Utku Çelik <utkuce@gmail.com>
2017-11-20 20:50:55 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ timedatectl set-ntp true
 
 arch="$1" bootp="$2" pass="$3"
 
-mkfs.ext4 arch
+mkfs.ext4 $arch
 mount $arch /mnt
 mkdir /mnt/boot
 mount $bootp /mnt/boot
@@ -13,9 +13,9 @@ mount $bootp /mnt/boot
 pacstrap /mnt base
 genfstab -U /mnt >> /mnt/etc/fstab
 
-sed -i '1ipass='$pass after_chroot.sh
-sed -i '1ibootp='$bootp after_chroot.sh
-sed -i '1iarch='$arch after_chroot.sh
+sed -i '3ipass='$pass after_chroot.sh
+sed -i '3ibootp='$bootp after_chroot.sh
+sed -i '3iarch='$arch after_chroot.sh
 
 chmod +x after_chroot.sh
 mv after_chroot.sh /mnt
