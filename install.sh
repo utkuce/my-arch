65fce3e051fd85a590e4b1af90d40d2cb6f78146
Utku Çelik <utkuce@gmail.com>
2017-12-06 14:17:50 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,4 +25,10 @@ arch-chroot /mnt ./after_chroot.sh
 chmod +x after_reboot.sh
 mv after_reboot /mnt/root/
 
+read -p "Installaction complete. Reboot now? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+   reboot
+fi
+
 reboot
