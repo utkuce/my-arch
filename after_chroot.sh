454932275cd49625e5ed1a171f892d06a81efa1d
Utku Çelik <utkuce@gmail.com>
2017-12-12 18:01:52 +0000

    Update after_chroot.sh

--- after_chroot.sh
+++ after_chroot.sh
@@ -47,13 +47,14 @@ pacman -S rofi xdg-utils --noconfirm
 
 # install pacaur
 mkdir -p /home/utku/pacaur_install
+chown -R utku /home/utku/pacaur_install utku
 cd /home/utku/pacaur_install
 pacman -S expac yajl git --noconfirm --needed
 curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower
 su -c "makepkg PKGBUILD --skippgpcheck --install --needed" utku
 curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur
 su -c "makepkg PKGBUILD --install --needed" utku
-rm -r //home/utku/pacaur_install
+rm -r /home/utku/pacaur_install
 pacaur -S i3-gaps
 
 systemctl enable NetworkManager.service
