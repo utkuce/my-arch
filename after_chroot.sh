161caaaa03cb77be5b946f698c81517a1fa77e4f
Utku Çelik <utkuce@gmail.com>
2017-12-13 12:45:53 +0000

    Update after_chroot.sh

--- after_chroot.sh
+++ after_chroot.sh
@@ -43,7 +43,7 @@ pacman -S networkmanager iw wpa_supplicant dhclient --noconfirm
 systemctl enable NetworkManager.service
 
 pacman -S xorg-server xorg-xinit xterm bspwm sxhkd --noconfirm
-#echo -e "setxkbmap tr\nexec bspwm" > /home/utku/.xinitrc
+echo -e "setxkbmap tr\nsxhkd&\nxterm&exec bspwm" > /home/utku/.xinitrc
 #echo "exec startx" >> /home/utku/.bash_profile
 
 pacman -S rofi xdg-utils --noconfirm
